e02c18edfb830bd34457e80ffaffb40ca4377042
chandaankita06-cmyk <chandaankita06@gmail.com>
2026-02-27 10:29:01 +0530

    Create printname.java

---
+++ printname.java
@@ -0,0 +1,15 @@
+class NamePrinter {
+    
+    void printName() {
+        System.out.println("Ankita");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        NamePrinter obj = new NamePrinter(); 
+        obj.printName();                      
+        
+    }
+}
